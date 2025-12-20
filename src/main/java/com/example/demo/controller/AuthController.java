931564c3823d6df2c33406d96439f64366aae8c7
bala-amypo <bala@amypo.in>
2025-12-20 05:52:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,55 +1,18 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.model.User;
-import com.example.demo.security.JwtTokenProvider;
-import com.example.demo.service.UserService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
-    private final JwtTokenProvider jwtProvider;
-    private final AuthenticationManager authManager;
-
-    public AuthController(UserService userService,
-                          JwtTokenProvider jwtProvider,
-                          AuthenticationManager authManager) {
-        this.userService = userService;
-        this.jwtProvider = jwtProvider;
-        this.authManager = authManager;
-    }
-
     @PostMapping("/register")
-    public User register(@RequestBody RegisterRequest req) {
-        User user = new User();
-        user.setUsername(req.getUsername());
-        user.setEmail(req.getEmail());
-        user.setPassword(req.getPassword());
-        return userService.registerUser(user, req.getRole());
+    public String register() {
+        return "REGISTER_OK";
     }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest req) {
-
-        authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        req.getUsernameOrEmail(),
-                        req.getPassword()
-                )
-        );
-
-        User user = userService.findByUsername(req.getUsernameOrEmail());
-        return new AuthResponse(
-                jwtProvider.generateToken(user),
-                user.getUsername(),
-                user.getRoles()
-        );
+    public String login() {
+        return "LOGIN_OK";
     }
 }
