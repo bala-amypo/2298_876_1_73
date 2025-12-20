51c4432c6c9ff6243c1e5518d12e426e606a7059
bala-amypo <bala@amypo.in>
2025-12-20 10:44:52 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,25 +1,25 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
-import io.swagger.v3.oas.models.security.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.*;
+// import io.swagger.v3.oas.models.security.*;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.info.Info;
 
 
-@Configuration
-public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-jwt",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
-                .info(new Info().title("Workflow Approval API").version("1.0"));
-    }
-}
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI()
+//                 .components(new Components()
+//                         .addSecuritySchemes("bearer-jwt",
+//                                 new SecurityScheme()
+//                                         .type(SecurityScheme.Type.HTTP)
+//                                         .scheme("bearer")
+//                                         .bearerFormat("JWT")))
+//                 .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+//                 .info(new Info().title("Workflow Approval API").version("1.0"));
+//     }
+// }
