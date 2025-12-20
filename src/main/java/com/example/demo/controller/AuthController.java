b1fbb0fddb836461e34192602ccd1256e1680655
bala-amypo <bala@amypo.in>
2025-12-20 10:50:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,18 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/register")
-    public String register() {
-        return "REGISTER_OK";
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping("/login")
-    public String login() {
-        return "LOGIN_OK";
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        userService.registerUser(user, request.getRole());
+
+        return "REGISTER_OK";
     }
 }
