479f9386e12251840c0e5b07a961ed9db03fd11f
bala-amypo <bala@amypo.in>
2025-12-20 11:26:52 +0000

    User commit

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -2,14 +2,20 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
-public class SecurityBeansConfig {
+public class WebSecurityConfig {
+
+    // Dummy AuthenticationManager bean
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
     }
 }
