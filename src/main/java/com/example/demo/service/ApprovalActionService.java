42e7b434e979257dde095c6556d7814b232cf623
bala-amypo <bala@amypo.in>
2025-12-24 15:54:08 +0000

    User commit

--- src/main/java/com/example/demo/service/ApprovalActionService.java
+++ src/main/java/com/example/demo/service/ApprovalActionService.java
@@ -1,12 +1,23 @@
 package com.example.demo.service;
 
+import com.example.demo.model.ApprovalAction;
 import com.example.demo.repository.ApprovalActionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ApprovalActionService {
     
     @Autowired
     private ApprovalActionRepository actionRepository;
+    
+    public ApprovalAction createAction(ApprovalAction action) {
+        return actionRepository.save(action);
+    }
+    
+    public List<ApprovalAction> getActionsByRequestId(Long requestId) {
+        return actionRepository.findByRequestId(requestId);
+    }
 }
