7e5b6a704381cfacb44e239f317f98c2a1b47d82
bala-amypo <bala@amypo.in>
2025-12-24 12:15:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApprovalRequestRepository.java
+++ src/main/java/com/example/demo/repository/ApprovalRequestRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.WorkflowTemplate;
+import com.example.demo.model.ApprovalRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface WorkflowTemplateRepository extends JpaRepository<WorkflowTemplate, Long> {
+public interface ApprovalRequestRepository extends JpaRepository<ApprovalRequest, Long> {
+    List<ApprovalRequest> findByRequesterId(Long requesterId);
 }
