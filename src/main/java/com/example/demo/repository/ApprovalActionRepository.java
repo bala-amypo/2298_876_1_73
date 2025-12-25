fc73b1d11e798220f3db94ea67596b67fb8c92f2
bala-amypo <bala@amypo.in>
2025-12-25 08:55:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApprovalActionRepository.java
+++ src/main/java/com/example/demo/repository/ApprovalActionRepository.java
@@ -1,17 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.ApprovalAction;
+import com.example.demo.model.ApprovalRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ApprovalActionRepository extends JpaRepository<ApprovalAction, Long> {
-    
-    @Query("SELECT a FROM ApprovalAction a WHERE a.levelNumber = :level AND a.action = :action")
-    List<ApprovalAction> findByLevelAndAction(@Param("level") Integer level, @Param("action") String action);
-    
-    List<ApprovalAction> findByRequestId(Long requestId);
+public interface ApprovalRequestRepository extends JpaRepository<ApprovalRequest, Long> {
+    List<ApprovalRequest> findByRequesterId(Long requesterId);
 }
